a0687ba889162d2dd0edb38136cb6ead9ec6d991
harsh-pandey001 <78418165+harsh-pandey001@users.noreply.github.com>
2022-07-08 11:05:59 +0530

    Css added

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Harsh pandey</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+      </div>
+    </header>
   )
 }
 
