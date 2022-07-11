fe3714b3e0b2c0f375c46acd7f7509d3ff001810
harsh-pandey001 <78418165+harsh-pandey001@users.noreply.github.com>
2022-07-11 10:19:43 +0530

    portfolio  and testimonial

--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,12 +1,89 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import img1 from "../../assets/portfolio1.jpg";
+import img2 from "../../assets/portfolio2.jpg";
+import img3 from "../../assets/portfolio3.jpg";
+import img4 from "../../assets/portfolio4.jpg";
+import img5 from "../../assets/portfolio5.png";
+import img6 from "../../assets/portfolio6.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: img1,
+    title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    github: "https://github.com/harsh-pandey001",
+    demo: "https://github.com/harsh-pandey001/myfirstgit",
+  },
+  {
+    id: 2,
+    image: img2,
+    title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    github: "https://github.com/harsh-pandey001",
+    demo: "https://github.com/harsh-pandey001/myfirstgit",
+  },
+  {
+    id: 3,
+    image: img3,
+    title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    github: "https://github.com/harsh-pandey001",
+    demo: "https://github.com/harsh-pandey001/myfirstgit",
+  },
+  {
+    id: 4,
+    image: img4,
+    title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    github: "https://github.com/harsh-pandey001",
+    demo: "https://github.com/harsh-pandey001/myfirstgit",
+  },
+  {
+    id: 5,
+    image: img5,
+    title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    github: "https://github.com/harsh-pandey001",
+    demo: "https://github.com/harsh-pandey001/myfirstgit",
+  },
+  {
+    id: 6,
+    image: img6,
+    title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    github: "https://github.com/harsh-pandey001",
+    demo: "https://github.com/harsh-pandey001/myfirstgit",
+  },
+];
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
-    Portfolio
-  </section>
-  )
-}
+    <section id="portfolio">
+      <h5> My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt= {title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn">
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
--- src/Components/testimonial/Testimonial.jsx
+++ src/Components/testimonial/Testimonial.jsx
@@ -1,8 +1,57 @@
 import React from "react";
 import "./testimonial.css";
+import av1 from '../../assets/avatar1.jpg';
+import av2 from '../../assets/avatar1.jpg';
+import av3 from '../../assets/avatar1.jpg';
+import av4 from '../../assets/avatar1.jpg';
+
 
 const Testimonial = () => {
-  return <section id="testimonial">Testimonial</section>;
+  return (
+    <section id="testimonial">
+      <h5>Review from client</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonial__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={av1} alt="connetion error"/>
+          </div>
+          <h5 className="client__name">Samuals</h5>
+            <small className="client__review">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam tempore aut vitae cum delectus quo illum non itaque accusantium dolorem! Itaque dolor in eum consequuntur vero necessitatibus totam atque eius?
+            </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={av1} alt="connetion error"/>
+          </div>
+          <h5 className="client__name">Samuals</h5>
+            <small className="client__review">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam tempore aut vitae cum delectus quo illum non itaque accusantium dolorem! Itaque dolor in eum consequuntur vero necessitatibus totam atque eius?
+            </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={av1} alt="connetion error"/>
+          </div>
+          <h5 className="client__name">Samuals</h5>
+            <small className="client__review">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam tempore aut vitae cum delectus quo illum non itaque accusantium dolorem! Itaque dolor in eum consequuntur vero necessitatibus totam atque eius?
+            </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={av1} alt="connetion error"/>
+          </div>
+          <h5 className="client__name">Samuals</h5>
+            <small className="client__review">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam tempore aut vitae cum delectus quo illum non itaque accusantium dolorem! Itaque dolor in eum consequuntur vero necessitatibus totam atque eius?
+            </small>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonial;
