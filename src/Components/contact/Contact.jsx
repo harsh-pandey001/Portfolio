a7cbf7f6b23dc4452328ef2fd0f698372f880546
harsh-pandey001 <78418165+harsh-pandey001@users.noreply.github.com>
2022-07-14 23:40:23 +0530

    contact form completed

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>harshpandey2060@gmail.com</h5>
+            <a href="mailto:harshpandey2060@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon"/>
+            <h4>Messenger</h4>
+            <h5>Harsh Pandey</h5>
+            <a
+              href="https://www.facebook.com/harsh.pandey.92505/"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon"/>
+            <h4>Whatsapp</h4>
+            <h5>7247569674</h5>
+            <a
+              href="http://api.whatsapp.com/send?phone=7247569674"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+
+        {/* END OF CONTACT OPTION */}
+
+        <form action="">
+          <input type="text" name="namr" placeholder="Your Full Name" required/>
+          <input type="email" name="email" placeholder="Your Email" required/>
+          <textarea name="message"rows="7" placeholder="Your message" required></textarea>
+        <button type="submit" className="btn btn-primary">Send Message</button>
+        
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/Components/testimonial/Testimonial.jsx
+++ src/Components/testimonial/Testimonial.jsx
@@ -1,10 +1,48 @@
 import React from "react";
 import "./testimonial.css";
-import av1 from '../../assets/avatar1.jpg';
-import av2 from '../../assets/avatar1.jpg';
-import av3 from '../../assets/avatar1.jpg';
-import av4 from '../../assets/avatar1.jpg';
+import av1 from "../../assets/avatar1.jpg";
+import av2 from "../../assets/avatar2.jpg";
+import av3 from "../../assets/avatar3.jpg";
+import av4 from "../../assets/avatar4.jpg";
 
+// import Swiper core and required modules
+import {Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
+
+const data = [
+  {
+    avatar: av1,
+    name: "jason",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quidem sed vel alias accusamus necessitatibus praesentium. Beatae, at possimus! Ab, debitis sapiente! Cumque hic nostrum quae itaque architecto iste ducimus!",
+  },
+  {
+    avatar: av2,
+    name: "rock",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quidem sed vel alias accusamus necessitatibus praesentium. Beatae, at possimus! Ab, debitis sapiente! Cumque hic nostrum quae itaque architecto iste ducimus!",
+  },
+  {
+    avatar: av3,
+    name: "harsh",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quidem sed vel alias accusamus necessitatibus praesentium. Beatae, at possimus! Ab, debitis sapiente! Cumque hic nostrum quae itaque architecto iste ducimus!",
+  },
+  {
+    avatar: av4,
+    name: "passion",
+    review:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quidem sed vel alias accusamus necessitatibus praesentium. Beatae, at possimus! Ab, debitis sapiente! Cumque hic nostrum quae itaque architecto iste ducimus!",
+  },
+];
 
 const Testimonial = () => {
   return (
@@ -12,44 +50,29 @@ const Testimonial = () => {
       <h5>Review from client</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonial__container">
-        <article className="testimonial">
+      <Swiper className="container testimonial__container"
+       // install Swiper modules
+       modules={[ Pagination]}
+       spaceBetween={40}
+       slidesPerView={1}
+       pagination={{ clickable: true }}
+      >
+        {
+          data.map(({avatar, name, review},index)=>{
+            return(
+              <SwiperSlide className="testimonial">
           <div className="client__avatar">
-            <img src={av1} alt="connetion error"/>
+            <img src={avatar}/>
           </div>
-          <h5 className="client__name">Samuals</h5>
-            <small className="client__review">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam tempore aut vitae cum delectus quo illum non itaque accusantium dolorem! Itaque dolor in eum consequuntur vero necessitatibus totam atque eius?
-            </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={av1} alt="connetion error"/>
-          </div>
-          <h5 className="client__name">Samuals</h5>
-            <small className="client__review">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam tempore aut vitae cum delectus quo illum non itaque accusantium dolorem! Itaque dolor in eum consequuntur vero necessitatibus totam atque eius?
-            </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={av1} alt="connetion error"/>
-          </div>
-          <h5 className="client__name">Samuals</h5>
-            <small className="client__review">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam tempore aut vitae cum delectus quo illum non itaque accusantium dolorem! Itaque dolor in eum consequuntur vero necessitatibus totam atque eius?
-            </small>
-        </article>
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={av1} alt="connetion error"/>
-          </div>
-          <h5 className="client__name">Samuals</h5>
-            <small className="client__review">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam tempore aut vitae cum delectus quo illum non itaque accusantium dolorem! Itaque dolor in eum consequuntur vero necessitatibus totam atque eius?
-            </small>
-        </article>
-      </div>
+          <h5 className="client__name">{name}</h5>
+          <small className="client__review">
+           {review}
+          </small>
+        </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
   );
 };
