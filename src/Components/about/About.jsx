16f55a8769a444bb8ff323a5e024bf9a2dc7acb8
harsh-pandey001 <78418165+harsh-pandey001@users.noreply.github.com>
2022-07-19 11:31:32 +0530

    some improvements

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-// import ME from '../../assets/me-about.jpg'
+import ME from '../../assets/iiii1.png'
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
@@ -14,7 +14,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            {/* <img src={ME} alt="About Image" /> */}
+            <img src={ME} alt="About Image" />
           </div>
         </div>
         <div className="about__content">
